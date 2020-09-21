53ae521cd7d922e853a65dd3db99d995cfd23169
Megan Henry <megangabriellehenry@gmail.com>
2020-09-21 14:00:47 -0500

    replaced HEAD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import NoodleComponent from './NoodleComponent'
-import EmersonComponent from './EmersonComponent'
 
+<<<<<<< HEAD
 class App extends Component {
   // runQueue=()=>{
   //   window.QueueIt.validateUser();
@@ -28,4 +27,27 @@ class App extends Component {
         )
     }
 }
+=======
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+>>>>>>> parent of f7d871a... set up diff pages for diff integration
 export default App;
