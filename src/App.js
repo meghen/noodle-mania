dc0bc2aed3cbb1c9ca054124d631b77ff11efe02
Megan Henry <megangabriellehenry@gmail.com>
2020-09-22 11:32:08 -0500

    Revert "forgot some queue code in there"
    This reverts commit 5c03146046062c9ecbbfafef42ed4263c9574e4b.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< HEAD
 class App extends Component {
   // runQueue=()=>{
   //   window.QueueIt.validateUser();
@@ -27,27 +26,4 @@ class App extends Component {
         )
     }
 }
-=======
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
->>>>>>> parent of f7d871a... set up diff pages for diff integration
 export default App;
