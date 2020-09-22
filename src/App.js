a97b2c212bd5895687639f01e5c8d7c4d477b659
Megan Henry <megangabriellehenry@gmail.com>
2020-09-22 11:39:54 -0500

    was missing my imports, lost in revert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, Component} from 'react';
+// import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import NoodleComponent from './NoodleComponent'
+import EmersonComponent from './EmersonComponent'
 
 class App extends Component {
   // runQueue=()=>{
