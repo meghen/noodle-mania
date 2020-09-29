99b32ca72f5f17e6a57c519df90ebf3f82d70550
Megan Henry <53824948+meghen@users.noreply.github.com>
2020-09-28 22:20:38 -0500

    removed validate user b/c it's serverside

--- src/EmersonComponent.js
+++ src/EmersonComponent.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 // import Emerson from './Emerson.jpg'
 
 class EmersonComponent extends Component {
-      componentDidMount = () => {
-        console.log('checking window', window);
-        window.QueueIt.validateUser();
-    }
+     // componentDidMount = () => {
+       // console.log('checking window', window);
+        //window.QueueIt.validateUser();
+   // }
     render() {
         return (
             <div>
