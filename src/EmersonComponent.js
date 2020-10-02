497bb249156e58d85bd761fda1be925b1b08f33f
Megan Henry <megangabriellehenry@gmail.com>
2020-10-02 11:03:46 -0500

    added mac daddy snippet, commented out validateUser

--- src/EmersonComponent.js
+++ src/EmersonComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 // import Emerson from './Emerson.jpg'
-import ArtistListItem from './ArtistListItem';
-import axios from 'axios';
+// import ArtistListItem from './ArtistListItem';
+// import axios from 'axios';
 import {connect} from 'react-redux';
 
 class EmersonComponent extends Component {
@@ -9,42 +9,42 @@ class EmersonComponent extends Component {
     //    console.log('checking window', window);
     //    window.QueueIt.validateUser();
     // }
-    componentDidMount() { // react Component method
-        console.log('checking window', window);
-        window.QueueIt.validateUser();
-        this.refreshArtists();
-      }
+    // componentDidMount() { // react Component method
+    //     console.log('checking window', window);
+    //     window.QueueIt.validateUser();
+    //     this.refreshArtists();
+    //   }
     
-      refreshArtists = () => {
-        // just like $.ajax()
-        axios({
-          method: 'GET',
-          url: '/#/emerson'
-        }).then((response) => {
-          console.log(response);
-          // response.data will be the array of artists
-          this.props.dispatch({
-            type: 'GET_ARTISTS',
-            payload: response.data
-          })
-        }).catch((err)=>{
-        console.log('error: ', err)
-        });
-    }
-        createArtistList() {
-            let artistListForDom = [];
-            for(let i = 0; i < this.props.reduxState.artistReducer.length; i += 1) {
-                let artist = this.props.reduxState.artistReducer[i];
-                let artistRow = (<ArtistListItem key={i} refreshArtists={this.props.refreshArtists} artist={artist} />);
-                artistListForDom.push(artistRow);
-            }
-            return artistListForDom;
-        }
+    //   refreshArtists = () => {
+    //     // just like $.ajax()
+    //     axios({
+    //       method: 'GET',
+    //       url: '/#/emerson'
+    //     }).then((response) => {
+    //       console.log(response);
+    //       // response.data will be the array of artists
+    //       this.props.dispatch({
+    //         type: 'GET_ARTISTS',
+    //         payload: response.data
+    //       })
+    //     }).catch((err)=>{
+    //     console.log('error: ', err)
+    //     });
+    // }
+    //     createArtistList() {
+    //         let artistListForDom = [];
+    //         for(let i = 0; i < this.props.reduxState.artistReducer.length; i += 1) {
+    //             let artist = this.props.reduxState.artistReducer[i];
+    //             let artistRow = (<ArtistListItem key={i} refreshArtists={this.props.refreshArtists} artist={artist} />);
+    //             artistListForDom.push(artistRow);
+    //         }
+    //         return artistListForDom;
+    //     }
     render() {
         return (
             <div>
                 <h1>THIS IS EMERSONS PAGE</h1>
-                {this.createArtistList()}
+                {/* {this.createArtistList()} */}
             </div>
         )
     }
