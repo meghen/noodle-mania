e756018ae6eec8e0a48fda40d98f20d07e2e7a7b
Megan Henry <megangabriellehenry@gmail.com>
2020-10-16 10:54:52 -0500

    moved validateuser to .then in axios

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import EmersonComponent from './EmersonComponent'
 import {connect} from 'react-redux';
 
 class App extends Component {
+  handleClick=()=>{
+    //also route to a new page
+    // this.history.push('/emerson')
+    console.log('is there history?', this.history);
+    
+  }
     render() {
         return (
             <div>
                 <Router>
-      <div className="App">
-        <h1>Who will it be?</h1>
-        <ul>
-          <li><Link to='/noodle'>Noodle</Link></li>
-          <li><Link to='/emerson'>Emerson</Link></li>
-        </ul>
-        <Route exact path='/noodle' component={NoodleComponent}/>
-        <Route exact path='/emerson' component={EmersonComponent}/>
-      </div>
-    </Router>
+                  <div className="App">
+                    <h1>Who will it be?</h1>
+                    <ul>
+                      <li><Link to='/noodle'>Noodle</Link></li>
+                      <li onClick={this.handleClick}><Link to='/emerson'>Emerson</Link></li>
+                    </ul>
+                    <Route exact path='/noodle' component={NoodleComponent}/>
+                    <Route exact path='/emerson' component={EmersonComponent}/>
+                  </div>
+                </Router>
             </div>
         )
     }
--- src/EmersonComponent.js
+++ src/EmersonComponent.js
@@ -14,8 +14,10 @@ class EmersonComponent extends Component {
     // }
     componentDidMount() { // react Component method
         // console.log('checking window', window);
-        window.QueueIt.validateUser();
+        // window.QueueIt.validateUser();
         this.refreshArtists();
+        console.log('history in component did mount', this.history);
+        
       }
     
       refreshArtists = () => {
@@ -24,6 +26,7 @@ class EmersonComponent extends Component {
           method: 'GET',
           url: '/test'
         }).then((response) => {
+          window.QueueIt.validateUser();
           console.log(response.data);
           this.setState({
               quote: response.data.quote
