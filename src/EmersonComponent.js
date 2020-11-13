568861cd521a67eb6fea2f257a9fe73ff6177cf2
Megan Henry <megangabriellehenry@gmail.com>
2020-11-13 14:57:18 -0600

    commenting out SPA support

--- src/EmersonComponent.js
+++ src/EmersonComponent.js
@@ -5,50 +5,50 @@ import axios from 'axios';
 import {connect} from 'react-redux';
 
 class EmersonComponent extends Component {
-    state = {
-        quote: ''
-    }
+    // state = {
+    //     quote: ''
+    // }
     //   componentDidMount = () => {
     //    console.log('checking window', window);
     //    window.QueueIt.validateUser();
     // }
-    componentDidMount() { // react Component method
-        // console.log('checking window', window);
-        // window.QueueIt.validateUser();
-        this.refreshArtists();
-        console.log('history in component did mount', this.history);
+    // componentDidMount() { // react Component method
+    //     // console.log('checking window', window);
+    //     // window.QueueIt.validateUser();
+    //     this.refreshArtists();
+    //     console.log('history in component did mount', this.history);
         
-      }
+    //   }
     
-      refreshArtists = () => {
-        console.log('clicked emerson fired');
-        axios({
-          method: 'GET',
-          url: '/test'
-        }).then((response) => {
-          console.log(response.data);
-          this.setState({
-              quote: response.data.quote
-            })
-        }).catch((err)=>{
-        console.log('error: ', err)
-        });
-    }
-        createArtistList() {
-            let artistListForDom = [];
-            for(let i = 0; i < this.props.reduxState.artistReducer.length; i += 1) {
-                let artist = this.props.reduxState.artistReducer[i];
-                let artistRow = (<ArtistListItem key={i} refreshArtists={this.props.refreshArtists} artist={artist} />);
-                artistListForDom.push(artistRow);
-            }
-            return artistListForDom;
-        }
+    //   refreshArtists = () => {
+    //     console.log('clicked emerson fired');
+    //     axios({
+    //       method: 'GET',
+    //       url: '/test'
+    //     }).then((response) => {
+    //       console.log(response.data);
+    //       this.setState({
+    //           quote: response.data.quote
+    //         })
+    //     }).catch((err)=>{
+    //     console.log('error: ', err)
+    //     });
+    // }
+    //     createArtistList() {
+    //         let artistListForDom = [];
+    //         for(let i = 0; i < this.props.reduxState.artistReducer.length; i += 1) {
+    //             let artist = this.props.reduxState.artistReducer[i];
+    //             let artistRow = (<ArtistListItem key={i} refreshArtists={this.props.refreshArtists} artist={artist} />);
+    //             artistListForDom.push(artistRow);
+    //         }
+    //         return artistListForDom;
+    //     }
     render() {
         return (
             <div>
                 <h1>THIS IS EMERSONS PAGE</h1>
-                {this.state.quote}
-                {this.createArtistList()}
+                {/* {this.state.quote}
+                {this.createArtistList()} */}
             </div>
         )
     }
--- src/NoodleComponent.js
+++ src/NoodleComponent.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 // import Emerson from './Emerson.jpg'
 
 class NoodleComponent extends Component {
-    componentDidMount = () => {
-        // console.log('checking window', window);
-        window.QueueIt.validateUser();
-    }
+    // componentDidMount = () => {
+    //     // console.log('checking window', window);
+    //     window.QueueIt.validateUser();
+    // }
     render() {
         return (
             <div>
